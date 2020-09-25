2effdcee5b87f3af6a555cfc74c34fc44f5e9e6c
gis-story <64885106+gis-story@users.noreply.github.com>
2020-09-25 17:02:14 +0700

    Add files via upload

---
+++ chart.js
@@ -0,0 +1,2 @@
+/*! jQuery v3.2.1 | (c) JS Foundation and other contributors | jquery.org/license */
+var chart_a = [['Task', 'Hours per Day'],['bb', 15],['Eat', 20],['Commute', 20],['Watch TV', 20],['Sleep', 70]]
