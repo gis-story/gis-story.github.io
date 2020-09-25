b75694c0ef0f042b98a735eb6bc38ab6d5f331d9
gis-story <64885106+gis-story@users.noreply.github.com>
2020-09-25 17:03:28 +0700

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,2 +1,2 @@
 /*! jQuery v3.2.1 | (c) JS Foundation and other contributors | jquery.org/license */
-var chart_a = [['Task', 'Hours per Day'],['bb', 15],['Eat', 20],['Commute', 20],['Watch TV', 20],['Sleep', 70]]
+var chart_a = [['Task', 'Hours per Day'],['bb', 150],['Eat', 20],['Commute', 20],['Watch TV', 20],['Sleep', 70]]
